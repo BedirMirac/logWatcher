5c1a9ba901f367792817fc8e28db026b8da7abfb
BedirMirac <mirac.bedir.tr@gmail.com>
2026-02-12 12:12:11 +0300

    Initial commit: Go-based Log Watcher

---
+++ README.md
@@ -0,0 +1,17 @@
+# LogWatcher
+
+LogWatcher is a powerful log monitoring tool written in Go. It enables users to efficiently watch, filter, and analyze logs in real-time. With its robust features, LogWatcher helps developers and system administrators gain insights into their applications and services, ensuring smooth operations and quick troubleshooting.
+
+## Features
+- **Real-time Log Monitoring**: Instantly see new log entries as they come in.
+- **Filtering and Searching**: Easily filter logs by keywords, severity, or date.
+- **Custom Alerts**: Set alerts for specific log patterns or thresholds.
+
+## Contributing
+We welcome contributions! Please read our contributing guidelines before submitting a pull request.
+
+## License
+LogWatcher is open-source software licensed under the MIT License.
+
+## Contact
+For support or inquiries, contact us at [your-email@example.com].
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+)
+
+const logPath = "/var/log/pacman.log" // the path of log file that you wanna watch
+
+func main() {
+	file := OpenFile()
+	defer file.Close()
+	file.Seek(0, io.SeekEnd)
+	ReadPacman(file)
+
+}
+
+func OpenFile() *os.File {
+	file, err := os.Open(logPath)
+	if err != nil {
+		panic(err)
+	}
+	return file
+}
+
+func ReadPacman(file *os.File) {
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				time.Sleep(500 * time.Millisecond)
+				continue
+			}
+			break
+
+		}
+		processLine(line)
+	}
+}
+
+func processLine(line string) {
+	line = strings.TrimSpace(line)
+	lowerLine := strings.ToLower(line)
+
+	if strings.Contains(lowerLine, "warning") {
+		fmt.Printf("WARNING: %s\n", line)
+	} else if strings.Contains(lowerLine, "error") {
+		fmt.Printf("ERROR: %s\n", line)
+	}
+}
