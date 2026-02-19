0f78350d00403f1723fe8649bda8dc79a0ddc8d7
BedirMirac <mirac.bedir.tr@gmail.com>
2026-02-19 12:26:46 +0300

    feat: Now it is possible to watch more than 1 file at the same time

--- README.md
+++ README.md
@@ -7,11 +7,30 @@ LogWatcher is a powerful log monitoring tool written in Go. It enables users to 
 - **Filtering and Searching**: Easily filter logs by keywords, severity, or date.
 - **Custom Alerts**: Set alerts for specific log patterns or thresholds.
 
+## How to use
+
+### Using go run 
+You can use by typing your terminal `go run main.go` but it won't work if you are not in the project directory.
+
+### Using go build
+```
+git clone https://github.com/BedirMirac/logWatcher.git
+cd logWatcher
+go build .
+```
+
+After these steps you can start the program by typing `logWatcher` in your terminal if you are in the project directory.
+
+### Using go install (RECOMMENDED)
+```
+git clone https://github.com/BedirMirac/logWatcher.git
+cd logWatcher
+go install
+```
+It is look like go build method but the difference is now you can start the program  by typing `logWatcher` in your terminal without being in the project directory.
+
 ## Contributing
 We welcome contributions! Please read our contributing guidelines before submitting a pull request.
 
 ## License
 LogWatcher is open-source software licensed under the MIT License.
-
-## Contact
-For support or inquiries, contact us at [your-email@example.com].
--- main.go
+++ main.go
@@ -6,40 +6,37 @@ import (
 	"io"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
-const logPath = "/var/log/pacman.log" // the path of log file that you wanna watch
+var wg sync.WaitGroup
 
 func main() {
-	file := OpenFile()
-	defer file.Close()
-	file.Seek(0, io.SeekEnd)
-	ReadPacman(file)
+	logFiles := []string{"/var/log/pacman.log", "text.txt"} // update this slices for yourself // doesn't matter how many files you wanna add
 
-}
-
-func OpenFile() *os.File {
-	file, err := os.Open(logPath)
-	if err != nil {
-		panic(err)
+	for _, file := range logFiles {
+		wg.Add(1)
+		go watcher(file)
 	}
-	return file
+
+	wg.Wait()
 }
 
-func ReadPacman(file *os.File) {
+func Read(file *os.File, wg *sync.WaitGroup) {
+	defer wg.Done()
 	reader := bufio.NewReader(file)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
-				time.Sleep(500 * time.Millisecond)
+				time.Sleep(500 * time.Millisecond) // if there is no line added after this program started, wait 500 ms to check again
 				continue
 			}
 			break
 
 		}
-		processLine(line)
+		processLine(line) // Sending line to function to see if the word we are looking for in the line or not
 	}
 }
 
@@ -51,5 +48,19 @@ func processLine(line string) { // where we print the messages as soon as we fin
 		fmt.Printf("WARNING: %s\n", line)
 	} else if strings.Contains(lowerLine, "error") {
 		fmt.Printf("ERROR: %s\n", line)
+	} else if strings.Contains(lowerLine, "installed") {
+		fmt.Printf("INSTALLED: %s\n", line)
+	} else if strings.Contains(lowerLine, "removed") {
+		fmt.Printf("REMOVED: %s\n", line)
 	}
+}
+
+func watcher(file string) { // opens log files heading the end of the file and do progress
+	log, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+	defer log.Close()
+	log.Seek(0, io.SeekEnd)
+	Read(log, &wg)
 }
